1cd78bc2712d754e83844b6ea0b366fbfc7dcce1
Artem <artfed@ttu.ee>
2023-03-03 19:03:24 +0200

    added implemenatation of constructor and push() method

--- src/LongStack.java
+++ src/LongStack.java
@@ -1,51 +1,60 @@
 
 public class LongStack {
-
+   private LongStack top;
+   long data;
+   LongStack next;
    public static void main (String[] argum) {
-      // TODO!!! Your tests here!
+
    }
 
    LongStack() {
-      // TODO!!! Your constructor here!
+      this.top = null;
    }
 
    @Override
    public Object clone() throws CloneNotSupportedException {
-      return this; // TODO!!! Your code here!
+      return this;
    }
 
    public boolean stEmpty() {
-      return false; // TODO!!! Your code here!
+      return false;
    }
 
    public void push (long a) {
-      // TODO!!! Your code here!
+      LongStack newNode = new LongStack();
+      newNode.data = a;
+
+      if (top != null) {
+         newNode.next = top;
+      }
+      top = newNode;
+
    }
 
    public long pop() {
-      return 0; // TODO!!! Your code here!
-   } // pop
+      return 0;
+   }
 
    public void op (String s) {
-      // TODO!!!
+
    }
-  
+
    public long tos() {
-      return 0; // TODO!!! Your code here!
+      return 0;
    }
 
    @Override
    public boolean equals (Object o) {
-      return true; // TODO!!! Your code here!
+      return true;
    }
 
    @Override
    public String toString() {
-      return null; // TODO!!! Your code here!
+      return null;
    }
 
    public static long interpret (String pol) {
-      return 0; // TODO!!! Your code here!
+      return 0;
    }
 
 }
